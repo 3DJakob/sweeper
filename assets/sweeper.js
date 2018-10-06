3c608ba85c4efa4935b720f5a134fb14e38b1b9d
Jakob <jakob@folkdatorn.se>
2018-10-06 13:38:02 +0200

    Design overhul and game improvments

--- assets/sweeper.js
+++ assets/sweeper.js
@@ -1,10 +1,18 @@
 let gameBoard = []
+let gameFrozen = false
 const sizeX = 10
 const sizeY = 10
 
 function initSweep () {
+  gameFrozen = false
+  clearGameBoard()
   initGameBoard()
   renderGameBoard()
+}
+
+function clearGameBoard () {
+  const target = document.getElementById('sweeperElement')
+  target.innerHTML = ''
 }
 
 function initGameBoard () {
@@ -14,20 +22,9 @@ function initGameBoard () {
   }
 
   for (let i = 0; i < gameBoard.length; i++) {
-    let sum = 0
-    if (!isTop(i)) {
-      if (getRowIndex(i - sizeX - 1) === getRowIndex(i - sizeX)) {
-        if (gameBoard[i - sizeX - 1].bomb) { sum++ }
-      }
-      if (gameBoard[i - sizeX].bomb) { sum++ }
-      if (getRowIndex(i - sizeX + 1) === getRowIndex(i - sizeX)) {
-        if (gameBoard[i - sizeX + 1].bomb) { sum++ }
-      }
-    }
-    // console.log(sum)
+    let sum = bombCount(i, 'top') + sideBombCount(i) + bombCount(i, 'bottom')
     gameBoard[i].number = sum
   }
-  console.log(gameBoard)
 }
 
 function isLeft (i) {
@@ -35,19 +32,58 @@ function isLeft (i) {
 }
 
 function isRight (i) {
-  return i % sizeX === 9
+  return i % sizeX === sizeX - 1
 }
 
 function isTop (i) {
-  return i <= sizeX
+  return i < sizeX
 }
 
 function isBottom (i) {
-  return i > sizeX * sizeY - sizeX
+  return i + 1 > sizeX * sizeY - sizeX
+}
+
+function bombCount (i, side) {
+  let topOrBottom = false
+  let steps = 0
+  if (side === 'top') {
+    topOrBottom = isTop(i)
+    steps = -sizeX
+  } else if (side === 'bottom') {
+    topOrBottom = isBottom(i)
+    steps = sizeX
+  }
+  let sum = 0
+  if (!topOrBottom) {
+    if (getRowIndex(i + steps - 1) === getRowIndex(i + steps)) {
+      if (gameBoard[i + steps - 1].bomb) { sum++ }
+    }
+    if (gameBoard[i + steps].bomb) { sum++ }
+    if (getRowIndex(i + steps + 1) === getRowIndex(i + steps)) {
+      if (gameBoard[i + steps + 1].bomb) { sum++ }
+    }
+  }
+  return sum
+}
+
+function sideBombCount (i) {
+  let sum = 0
+  if (!isLeft(i) && gameBoard[i - 1].bomb) {
+    sum++
+  }
+  console.log(i)
+  if (!isRight(i) && gameBoard[i + 1].bomb) {
+    sum++
+  }
+  return sum
 }
 
 function getRowIndex (i) {
   let res = 0
+  if (i < 0) {
+    return -1
+  }
+  i++
   while (i - sizeX > 0) {
     i = i - sizeX
     res++
@@ -56,7 +92,7 @@ function getRowIndex (i) {
 }
 
 function randomBool () {
-  return Boolean(Math.round(Math.random()));
+  return Boolean(Math.round(Math.random() - 0.3))
 }
 
 function renderGameBoard () {
@@ -70,16 +106,63 @@ function renderGameBoard () {
 function renderRow (rowNumber) {
   const row = document.createElement('div')
   for (let i = 0; i < sizeX; i++) {
-    const obj = gameBoard[i + rowNumber * 10]
+    const obj = gameBoard[i + rowNumber * sizeX]
     const square = document.createElement('div')
-    square.textContent = obj.number
-    if (obj.bomb) {
-      square.classList.add('bomb')
-      // square.textContent = 'B'
-    }
+    square.addEventListener('click', function () {
+      click(obj, square, true)
+    })
+    square.addEventListener('contextmenu', function (evt) {
+      evt.preventDefault()
+      click(obj, square, false)
+    })
     row.appendChild(square)
   }
   return row
+}
+
+function click (obj, element, leftClick) {
+  if (!gameFrozen) {
+    if (leftClick) {
+      if (obj.bomb) {
+        showGameBoard()
+        window.alert('You lost!')
+        gameFrozen = true
+      } else {
+        element.textContent = obj.number
+        obj.clicked = true
+      }
+    } else {
+      element.innerHTML = '<i class="fa fa-flag-o" aria-hidden="true"></i>'
+    }
+    if (checkVictory()) {
+      window.alert('You win!')
+      gameFrozen = true
+    }
+  }
+}
+
+function showGameBoard () {
+  for (let i = 0; i < gameBoard.length; i++) {
+    const obj = gameBoard[i]
+    const selector = '#sweeperElement div:nth-child(' + (getRowIndex(i) + 1) + ') div:nth-child(' + (i % sizeX + 1) + ')'
+    const target = document.querySelector(selector)
+    if (obj.bomb) {
+      target.classList.add('bomb')
+      target.innerHTML = '<i class="fa fa-bomb" aria-hidden="true"></i>'
+    } else {
+      target.textContent = obj.number
+    }
+  }
+}
+
+function checkVictory () {
+  let winning = true
+  gameBoard.forEach(function (square) {
+    if (!(square.bomb || square.clicked)) {
+      winning = false
+    }
+  })
+  return winning
 }
 
 window['initSweep'] = initSweep
